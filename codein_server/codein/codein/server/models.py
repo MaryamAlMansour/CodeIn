b880302534ef7d51b14959a3ac9d7c2e15e6c8f7
zankuda <zankuda@stevens.edu>
2018-02-14 00:15:22 -0500

    Initial commit for server code. Testing collaboration capabilities.

---
+++ codein_server/codein/codein/server/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=12)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    developer = models.BooleanField(default=False)
+    python = models.BooleanField(default=False)
+    java = models.BooleanField(default=False)
+    standard_c = models.BooleanField(default=False)
+    c_plus_plus = models.BooleanField(default=False)
+    other_language = models.CharField(max_length=12)
+    
