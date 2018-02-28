ad60afdfaa8e926dac860e70846dc9e06ebe9ae4
zankuda <zankuda@stevens.edu>
2018-02-27 23:09:32 -0500

    Added the rest-auth utility for Django REST framework addition. Enables user creation, login, logout, and cookie renewal.

--- codein_server/codein/codein/urls.py
+++ codein_server/codein/codein/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from rest_framework_jwt.views import refresh_jwt_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^', include('rest_auth.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^refresh-token/', refresh_jwt_token),
 ]
