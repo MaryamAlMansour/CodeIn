f7f42f7ba8aa59b4b735f5eab6f9e7408b26c165
Allo0o2a <31714177+Allo0o2a@users.noreply.github.com>
2018-03-23 18:19:30 -0400

    Update urls.py

--- codein_server/codein/platforms/urls.py
+++ codein_server/codein/platforms/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from .views import ProjectView
+from .views import ProjectView, PortfoliotView
 from django.conf.urls import url
 
 router = routers.SimpleRouter()
 router.register(r'project', ProjectView)
+router.register(r'portfolio', PortfoliotView)
 
 urlpatterns = router.urls
 
