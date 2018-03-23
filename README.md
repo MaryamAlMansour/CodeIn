bc68ff5ea84891729f33012962f7f931e7ea634f
MaryamAlMansour <eng.maryam.al.mansour@gmail.com>
2018-03-23 13:57:12 -0400

    Directory Clean Up

--- README.md
+++
@@ -1,4 +0,0 @@
-# 695 / CodeIn
-CodeIn, a platform for coders to network and freelance. 
-Built using React framework for front-end and Django frmework for back-end. 
-
--- codein_server/codein/platforms/serializers.py
+++ codein_server/codein/platforms/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Portfolio
+
+
+class PortfolioSerializer(serializers.ModelSerializer):
+
+    user = serializers.Field('user.username')
+
+    class Meta:
+        model = Portfolio
+        # you can either include all the fields from models, or excludes the ones you don't need.
+        exclude = ['created_at', 'updated_at']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
 
-    # = serializers.CharField(required=True)
+    user = serializers.Field('user.username')
+
     class Meta:
         model = Project
         # you can either include all the fields from models, or excludes the ones you don't need.
