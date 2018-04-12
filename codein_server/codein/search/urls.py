a551901e77d2a10affde9de6fda89525f36ae58e
MAMansour <eng.maryam.al.mansour@gmail.com>
2018-04-12 18:47:40 -0400

    Delete urls.py

--- codein_server/codein/search/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .views import SearchView
-from django.conf.urls import url
-
-router = routers.SimpleRouter()
-router.register('filter', SearchView)
-
-urlpatterns = router.urls
