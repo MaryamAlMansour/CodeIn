63d9dc499ea31fd48fbcdcc4a7a26ab0b5601989
MAMansour <eng.maryam.al.mansour@gmail.com>
2018-04-26 14:00:38 -0400

    Update urls.py

--- codein_server/codein/server/urls.py
+++ codein_server/codein/server/urls.py
@@ -6,7 +6,7 @@ from .views import UserViewset
 from django.conf.urls import url
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewset)
+router.register(r'users/', UserViewset)
 router.register(r'users/get_search_user/(?P<search_user_name>\w+)/', UserViewset)
 router.register(r'users/get_search_user/(?P<search_email>\w+)/', UserViewset)
 
